c2dd13ca89730e4391e248bae2ee75a630104cab
aunnaqvi <aunnaqvi747@gmail.com>
2023-05-10 12:03:44 +0500

    code change commit
    10-MAY-2023
    1) Make Api Config for (getNewUserRequests, saveUserSecurityAdmin, editUserSecurityAdmin and getAllUserList).
    2) Make Action Types for (getNewUserRequests, saveUserSecurityAdmin, editUserSecurityAdmin and getAllUserList).

--- src/commen/apis/Api_config.js
+++ src/commen/apis/Api_config.js
@@ -14,9 +14,29 @@ const getNewUserRequestsCounts = {
   RequestMethod: "ServiceManager.GetNewUserRequestsCount",
 };
 
+const getNewUserRequests = {
+  RequestMethod: "ServiceManager.GetNewUserRequests",
+};
+
+const saveUserSecurityAdmin = {
+  RequestMethod: "ServiceManager.SaveUser",
+};
+
+const editUserSecurityAdmin = {
+  RequestMethod: "ServiceManager.EditUser",
+};
+
+const getAllUserList = {
+  RequestMethod: "ServiceManager.GetAllUsersList",
+};
+
 export {
   authenticationLogIn,
   authenticationSignUp,
   authenticationRefreshToken,
   getNewUserRequestsCounts,
+  getNewUserRequests,
+  saveUserSecurityAdmin,
+  editUserSecurityAdmin,
+  getAllUserList,
 };
